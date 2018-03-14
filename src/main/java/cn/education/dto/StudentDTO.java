8ee1cca4b7d6cdddce060a1555a8c97d6642c1f2
Administrator <Administrator@HSNWI6CHELRTK6U.lan>
2018-03-14 23:03:20 +0800

    添加学生基础信息类

---
+++ src/main/java/cn/education/dto/StudentDTO.java
@@ -0,0 +1,32 @@
+package cn.education.dto;
+
+public class StudentDTO {
+	private String stdId;
+	private String stdName;
+	private Integer age;
+
+	public String getStdId() {
+		return stdId;
+	}
+
+	public void setStdId(String stdId) {
+		this.stdId = stdId;
+	}
+
+	public String getStdName() {
+		return stdName;
+	}
+
+	public void setStdName(String stdName) {
+		this.stdName = stdName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
